90e870eafa1bfc4a1f665c0aa9029ea7fbf0b31c
Anmol Dua <33629087+charliedua@users.noreply.github.com>
2026-01-09 08:15:33 +1100

    Create stack.c

---
+++ utils/c/stack/stack.c
@@ -0,0 +1,24 @@
+struct Stack {
+    int value;
+    struct Stack* prev;
+};
+
+void push(struct Stack** stack, int value) {
+    struct Stack* temp = malloc(sizeof(struct Stack));
+    temp->value = value;
+    temp->prev  = *stack;
+    *stack = temp;
+}
+
+void pop(struct Stack** stack, int** out) {
+    if(*stack == NULL) {
+        *out = NULL;
+        return;
+    }
+
+    **out = (*stack)->value;
+
+    struct Stack* temp = *stack;
+    *stack = (*stack)->prev;
+    free(temp);
+}
